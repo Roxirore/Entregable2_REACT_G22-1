6ebe20e1def1fdfe415142f67104df23ad7be1d9
Roxirore <roxana.rod.rev@gmail.com>
2023-01-30 16:41:55 -0500

    description

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -9,9 +9,17 @@ const WeatherCard = ({weather}) => {
             <h2 className="card__subtitle">City: {weather?.name}</h2>
             <h2 className="card__subtitle">Country: {weather?.sys.country}</h2>
         </header>
-        <section>
-            <img src={weather && `http://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`} alt="" />
-            <h3>{weather?.main.temp}</h3>
+        <section className="card__icon-container">
+            <img className="card__icon" src={weather && `http://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`} alt="" />
+            <h3 className="card__temp">{weather?.main.temp}</h3>
+        </section>
+        <section className="card__info">
+            <h3>{weather?.weather[0].description}</h3>
+            <ul>
+                <li><span></span></li>
+                <li><span></span></li>
+                <li><span></span></li>
+            </ul>
         </section>
 
     </article>
