8d78837b5d9f552a42a9ad9f181aa99b4148db1c
Roxirore <roxana.rod.rev@gmail.com>
2023-01-30 16:28:39 -0500

    header y seccion1

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 const WeatherCard = ({weather}) => {
     console.log(weather)
   return (
-    <div>WeatherCard Hoal</div>
+    <article className="card">
+        <header className="card__header">
+            <h1 className="card__title">Weather App</h1>
+            <h2 className="card__subtitle">City: {weather?.name}</h2>
+            <h2 className="card__subtitle">Country: {weather?.sys.country}</h2>
+        </header>
+        <section>
+            <img src={weather && `http://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`} alt="" />
+            <h3>{weather?.main.temp}</h3>
+        </section>
+
+    </article>
   )
 }
 
